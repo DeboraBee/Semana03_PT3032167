f4ef14da5956c8b56929aa6e3b6fca93be7de616
Debora <Debora>
2025-08-21 23:29:57 +0000

    conectando pythonanywhere e github

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,43 @@
 
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask! Bem vindos alunos do IFSP'
+def index():
+    return '<h1>Hello World</h1><h2>Disciplina PTBDSWS</h2>'
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>' .format(name)
+
+from flask import request
+
+@app.route('/contextorequisicao')
+def contexto_requisicao():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is{}</p>' .format(user_agent)
+
+@app.route('/codigostatusdiferente')
+def cod_status_diferente():
+    return '<p>Bad request</p>' , 400
+
+from flask import make_response
+
+@app.route('/objetoresposta')
+def objeto_resposta():
+    response = make_response('<h1>This document carries a cookie!</h1>' )
+    response.set_cookie('answer', '42')
+    return response
+
+from flask import redirect
+
+@app.route('/redirecionamento')
+def redirecionamento():
+    return redirect('https://ptb.ifsp.edu.br/')
+
+from flask import abort
+
+@app.route('/abortar')
+def abortar():
+    abort(404)
